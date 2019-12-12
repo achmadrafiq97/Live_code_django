802a07cf6dccb0763a2842609c7da9da3cf87882
achmadrafiq97 <rafiq@alterra.id>
2019-12-12 12:12:34 +0700

    add live_code_django

---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alta2/Documents/Folder live code/Live_code_django/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
